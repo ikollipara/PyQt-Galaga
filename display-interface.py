44d028d010de8caa953c171b7fd1d14789d3e1eb
Ian Kollipara <ian.kollipara@llwarriors.org>
2018-04-26 08:26:23 -0500

    Added Background and MenuBar to Main Window

--- display-interface.py
+++ display-interface.py
@@ -12,13 +12,40 @@ class GameWindow(QtWidgets.QMainWindow):
         self.display = MainWidget(self.controller)
         self.setCentralWidget(self.display)
         self.display.show()
+        self.create_actions()
+        self.create_menus()
 
     def setup_window(self):
         xSize = 1000
-        ySize = 1500
+        ySize = 900
         self.resize(xSize, ySize)
         desktop = QtWidgets.QDesktopWidget().screenGeometry()
         gameWindow = self.geometry()
+        xloc = (desktop.width() - gameWindow.width())/2
+        yLoc = (desktop.height() - gameWindow.height())/2
+        self.move(xloc, yLoc)
+        self.setWindowTitle('Atario')
+        self.setWindowIcon(QtGui.QIcon('.\Images\Window Icon.jpg'))
+
+    def create_actions(self):
+        self.aboutAction = QtWidgets.QAction("&About", self, statusTip="More information about the program", triggered=self.about)
+        self.scoresAction = QtWidgets.QAction('H&igh Scores', self, statusTip='Current High Scores', triggered=self.high_scores)
+
+    def create_menus(self):
+        self.menu = self.menuBar()
+        self.menu.addAction(self.scoresAction)
+        self.menu.addAction(self.aboutAction)
+
+    def quit(self):
+        self.close()
+
+    def about(self):
+        QtWidgets.QMessageBox.about(self, 'Atario',
+                                         """This program was developed by
+Ian, Tessa, and Collin over the course of 4 weeks""")
+
+    def high_scores(self):
+        QtWidgets.QMessageBox.about(self, 'Atario High Scores', """Test""")
 
 # region Test Main
 pyQtApp = QtWidgets.QApplication(sys.argv)

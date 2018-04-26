84b0689767eac8ede5d26bc400b2b41f5212a03a
collinrich <collin.rich@llwarriors.org>
2018-04-26 09:00:17 -0500

    Added key press event and open methods in controller

--- Controller.py
+++ Controller.py
@@ -1,3 +1,22 @@
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtCore import QEvent
+from display_view import MainWidget
+import sys
+
 class Controller(object):
     def __init__(self):
+        self.obstacles = []
+        self.ship = None
+
+    def move_right(self):
         pass
+
+    def move_left(self):
+        pass
+
+    def create_obstacle(self):
+        pass
+
+
+
+

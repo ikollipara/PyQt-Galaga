2b2d084c1456759c9e08c5a59c3b7cfd96aca235
Ian Kollipara <ian.kollipara@llwarriors.org>
2018-04-30 08:51:54 -0500

    Added Icons and Ship Display

--- display_view.py
+++ display_view.py
@@ -7,9 +7,10 @@ class MainWidget(QtWidgets.QWidget):
     def __init__(self, controller, parent=None):
         super(MainWidget, self).__init__(parent)
         self.controller = controller
+        self.layout = QtWidgets.QGraphicsGridLayout()
         self.background = QtGui.QImage(self.width(), self.height(), QtGui.QImage.Format_RGB32)
         self.background.fill(1)
-        self.ship = QtWidgets.QLabel()
+        self.ship = QtGui.QPixmap('.\Images\ShipIcon.PNG')
 
     def paintEvent(self, QPaintEvent):
         painter = QtGui.QPainter(self)
@@ -17,6 +18,7 @@ class MainWidget(QtWidgets.QWidget):
         pixSize = self.ship.size()
         self.background = self.background.scaled(size)
         painter.drawImage(0, 0, self.background)
+        painter.drawPixmap(self.controller.ship.loc[0], self.controller.ship.loc[1], self.ship)
 
     def keyPressEvent(self, event):
         if event.key() in [QtCore.Qt.Key_D]:

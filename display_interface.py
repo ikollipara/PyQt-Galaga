ff5adb675ff2fbe995b89caf78e1f441b0afd709
Ian Kollipara <ian.kollipara@llwarriors.org>
2018-05-04 08:06:14 -0500

    Removed Arrow Keys

--- display_interface.py
+++ display_interface.py
@@ -53,23 +53,23 @@ Ian, Tessa, and Collin over the course of 4 weeks""")
         QtWidgets.QMessageBox.about(self, 'Atario High Scores', """Test""")
 
     def keyPressEvent(self, event):
-        if event.key() in [QtCore.Qt.Key_D, QtCore.Qt.Key_Right]:
+        if event.key() in [QtCore.Qt.Key_D]:
             pixels = 15
             if self.controller.ship.x > self.width():
                 pixels = self.width() - self.controller.ship.x
             self.controller.move_ship_right(pixels)
             self.update()
-        elif event.key() in [QtCore.Qt.Key_A, QtCore.Qt.Key_Left]:
+        elif event.key() in [QtCore.Qt.Key_A]:
             pixels = 15
             if not self.controller.ship.x < 0:
                 self.controller.move_ship_left(pixels)
             self.update()
-        elif event.key() in [QtCore.Qt.Key_W, QtCore.Qt.Key_Up]:
+        elif event.key() in [QtCore.Qt.Key_W]:
             pixels = 15
             if not self.controller.ship.y < 0:
                 self.controller.move_ship_forward(pixels)
             self.update()
-        elif event.key() in [QtCore.Qt.Key_S, QtCore.Qt.Key_Down]:
+        elif event.key() in [QtCore.Qt.Key_S]:
             pixels = 15
             if self.controller.ship.y > self.height():
                 pixels = self.height() - self.controller.ship.y

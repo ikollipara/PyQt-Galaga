d0a8817bf9b1969608f002a436b2993ddb286629
collinrich <collin.rich@llwarriors.org>
2018-05-10 08:49:32 -0500

    Continued on collision detection

--- Controller.py
+++ Controller.py
@@ -1,1 +1,64 @@
-from PyQt5 import QtGui, QtCore, QtWidgetsfrom PyQt5.QtCore import QEventimport sysfrom Mobile import Mobilefrom Ship import Shipfrom World import Worldfrom random import randintclass Controller(object):    def __init__(self):        self.ship = Ship(None, 4, 4, 460, 770)        self.obstacles = []        self.world = World(self)        self.keys = []        self.world = World()        self.keys = {            "right" : False,            "left" : False,            "up" : False,            "down" : False}        self.screenWidth = None    def move_ship(self):        #        #   simplified into 1 method as opposed to 4        #        pixels = 10        directions = []        for key in self.keys:            if key.value == True:                directions.append(key)        for direction in directions:            while self.keys[direction] == True:                loc = self.ship.move(direction, pixels)                self.world.update_ship_position(loc, self.ship.height, self.ship.width)                self.ship.move(direction, pixels)    def create_obstacle(self):        sizes = [[100, 200], [200, 100], [50, 250], [250, 50], [100, 100], [50, 50], [50, 100], [100, 50]]        size = sizes[randint(0, 7)]        obstacle = Mobile(None, size[0], size[1], 0, 0)        self.world.update_obstacle_positions()        return obstacle    def create_ship(self):        image = "test.png"        ship = Ship(image, 10, 10, 500, 100)        self.ship = ship    def move_obstacles(self):        pixels = 10        for obs in self.obstacles:            obs.move(pixels)            self.world.update_obstacle_positions()            if obs.y > self.screenWidth:                self.obstacles.remove(obs)    def lose_life(self):        bool = self.world.detect_collision()        if bool:            self.ship.lose_life()        else:            pass
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtCore import QEvent
+import sys
+from Mobile import Mobile
+from Ship import Ship
+from World import World
+from random import randint
+
+class Controller(object):
+    def __init__(self):
+        self.ship = Ship(None, 4, 4, 460, 770)
+        self.obstacles = []
+        self.world = World(self)
+        self.keys = []
+        self.world = World(self)
+        self.keys = {
+            "right" : False,
+            "left" : False,
+            "up" : False,
+            "down" : False}
+        self.screenWidth = None
+
+    def move_ship(self):
+        #
+        #   simplified into 1 method as opposed to 4
+        #
+        pixels = 10
+        directions = []
+        for key in self.keys:
+            if key.value == True:
+                directions.append(key)
+        for direction in directions:
+            while self.keys[direction] == True:
+                loc = self.ship.move(direction, pixels)
+                self.world.update_ship_position(loc, self.ship.height, self.ship.width)
+                self.ship.move(direction, pixels)
+
+    def create_obstacle(self):
+        sizes = [[100, 200], [200, 100], [50, 250], [250, 50], [100, 100], [50, 50], [50, 100], [100, 50]]
+        size = sizes[randint(0, 7)]
+        obstacle = Mobile(None, size[0], size[1], 0, 0)
+        self.world.update_obstacle_positions()
+        return obstacle
+
+    def create_ship(self):
+        image = "test.png"
+        ship = Ship(image, 10, 10, 500, 100)
+        self.ship = ship
+
+    def move_obstacles(self):
+        pixels = 10
+        for obs in self.obstacles:
+            obs.move(pixels)
+            self.world.update_obstacle_positions()
+            if obs.y > self.screenWidth:
+                self.obstacles.remove(obs)
+
+    def lose_life(self):
+        bool = self.world.detect_collision()
+        if bool:
+            self.ship.lose_life()
+        else:
+            pass
+
--- World.py
+++ World.py
@@ -10,15 +10,15 @@ class World(object):
         """Finds the top right and bottom left coordinates or the object and appends them"""
         self.obstacleLocations = []
         for obstacle in self.controller.obstacles:
-            bottomleft = (int(obstacle.loc[0] - (obstacle.width / 2)), int(obstacle.loc[1] - (obstacle.height / 2)))
-            topRight = (int(obstacle.loc[0] + (obstacle.width / 2)), int(obstacle.loc[1] + (obstacle.height / 2)))
+            bottomleft = (int(obstacle.loc[0] - (obstacle.width / 2)), int(obstacle.loc[1] + (obstacle.height / 2)))
+            topRight = (int(obstacle.loc[0] + (obstacle.width / 2)), int(obstacle.loc[1] - (obstacle.height / 2)))
             locations = [bottomleft, topRight]
             self.obstacleLocations.append(locations)
 
     def update_ship_position(self, location, height, width):
         """Finds the top right and bottom left coordinates or the object and appends them"""
-        bottomleft = (int(location[0] - (width / 2)), int(location[1] - (height / 2)))
-        topRight = (int(location[0] + (width / 2)), int(location[1] + (height / 2)))
+        bottomleft = (int(location[0] - (width / 2)), int(location[1] + (height / 2)))
+        topRight = (int(location[0] + (width / 2)), int(location[1] - (height / 2)))
         self.shipLocations = [bottomleft, topRight]
 
     def detect_collision(self):
@@ -26,8 +26,9 @@ class World(object):
         for obstacleLocation in self.obstacleLocations:
             bottomLeftX = obstacleLocation[0][0]
             bottomLeftY = obstacleLocation[0][1]
-            if type(self.shipLocations) == ([]):
+            if type(self.shipLocations) == type([]):
                 if bottomLeftX >= self.shipLocations[0][0] and bottomLeftX <= self.shipLocations[1][0]:
+                    print("X")
                     if bottomLeftY >= self.shipLocations[1][1] and bottomLeftY <= self.shipLocations[0][1]:
                         collided = True
                         print("OOOOOOOOOOOOOO")

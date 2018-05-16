ae0dfb78d021aa1d6800015dce806c164ccfc79d
collinrich <collin.rich@llwarriors.org>
2018-05-16 08:29:43 -0500

    Started shooting

---
+++ Bullet.py
@@ -0,0 +1,14 @@
+from Mobile import Mobile
+class Bullet(Mobile):
+    def __init__(self, image, height, width, x, y, lives=1):
+        super().__init__(image, height, width, x, y, lives=1)
+        self.box = None
+
+    def move(self, pixels):
+        """Moves bullet on screen up toward obstacles"""
+        #
+        # Object forwards moves it up on screen towards obstacles, so self.y -= 1 instead of self.y += 1.
+        #
+        self.y -= pixels
+        self.loc = [self.x, self.y]
+        return self.loc
